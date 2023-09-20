f0c5c8b6867a8e7d3be634ec25bb78a2bf84d02b
willie prakasa <127827978+willieprakasa22@users.noreply.github.com>
2023-09-20 17:47:24 +0700

    Add files via upload

---
+++ Tulisan Berjalan/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Tulisan_Berjalan
+{
+    partial class frmTulisanBerjalan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblAnimasi = new System.Windows.Forms.Label();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblAnimasi
+            // 
+            this.lblAnimasi.AutoSize = true;
+            this.lblAnimasi.Location = new System.Drawing.Point(66, 23);
+            this.lblAnimasi.Name = "lblAnimasi";
+            this.lblAnimasi.Size = new System.Drawing.Size(82, 13);
+            this.lblAnimasi.TabIndex = 0;
+            this.lblAnimasi.Text = "Tulisan Berjalan";
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.Location = new System.Drawing.Point(163, 80);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(75, 23);
+            this.btnKeluar.TabIndex = 1;
+            this.btnKeluar.Text = "Keluar";
+            this.btnKeluar.UseVisualStyleBackColor = true;
+            this.btnKeluar.Click += new System.EventHandler(this.btnKeluar_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // frmTulisanBerjalan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKeluar);
+            this.Controls.Add(this.lblAnimasi);
+            this.Name = "frmTulisanBerjalan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tulisan Berjalan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAnimasi;
+        private System.Windows.Forms.Button btnKeluar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Tulisan Berjalan/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tulisan_Berjalan
+{
+    public partial class frmTulisanBerjalan : Form
+    {
+        public frmTulisanBerjalan()
+        {
+            InitializeComponent();
+        }
+
+       
+       
+
+        private void btnKeluar_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            lblAnimasi.Left -= 10;
+            if (lblAnimasi.Left < -lblAnimasi.Width) lblAnimasi.Left = this.Width;
+        }
+    }
+    }
+
